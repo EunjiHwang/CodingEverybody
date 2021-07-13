23b381dd5bc851a7ea21e5111d7c067769f2269f
EunjiHwang <1101j@naver.com>
2021-07-13 16:41:05 +0900

    6.5

---
+++ StringOperation.java
@@ -0,0 +1,10 @@
+
+public class StringOperation {
+	public static void main(String[] args) {
+		System.out.println("Hello World".length()); //11
+		// .replace(바꿀 문자열 부분), (replace할 문자열)
+		System.out.println("Hello, [[[name]]] ... bye. ".replace("[[[name]]]", "eunji"));
+		
+	}
+
+}
