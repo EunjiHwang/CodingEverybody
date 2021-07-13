d28d7518ef5f51afec969d9f567e50d2114668ac
EunjiHwang <1101j@naver.com>
2021-07-13 16:13:06 +0900

    6.2

---
+++ Datatype.java
@@ -0,0 +1,18 @@
+
+public class Datatype {
+	public static void main(String[] args) {
+		System.out.println(6); //Number
+		System.out.println("six"); //String
+		
+		System.out.println("6"); //String 6
+		
+		System.out.println(6+6); //12
+		System.out.println("6"+"6"); //66
+		
+		System.out.println(6*6); //36
+//		System.out.println("6"*"6"); 문자열끼리는 곱셈이 불가능하다.
+		
+		System.out.println("1111".length()); //4(문자열 길이)
+//		System.out.println(1111.length()); 숫자는 길이를 세는 것이 존재하지 않는다.
+	}
+}
